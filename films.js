6bfcb9b2f93aed992043fc832faf85597122da75
Nicolas Melin <nicem@hotmail.fr>
2021-09-27 09:50:11 +0200

    new

--- films.js
+++ films.js
@@ -1,5 +1,3 @@
-
-
 const baseUrl = "http://localhost:8000/api/v1/titles/"
 
 
@@ -8,20 +6,19 @@ fetch(baseUrl + '?sort_by=-imdb_score' )
     .then(data => {
         const bestFilmData = data.results[0];
         const id = bestFilmData.id
-
         fetch(baseUrl + id)
         .then (response => response.json())
         .then(dataMovie => {
-        const titleBest = document.getElementById("title-best");
+        const titleBest = document.getElementById("best-title");
         const bestDescription = document.getElementById("best-description");
-        const imageBest = document.getElementById("image-best");
-
+        const imageBest = document.getElementById("best-image");
         titleBest.innerHTML = dataMovie.title;
         bestDescription.innerHTML = dataMovie.long_description;
         imageBest.src = dataMovie.image_url;
         })})
 
 function getBestMovies(){
+     const bestMovie = document.getElementById('best-score')
      fetch(baseUrl + '?sort_by=-imdb_score')
     .then(response => response.json())
     .then(data => {
@@ -29,73 +26,66 @@ function getBestMovies(){
         for( movie of movies){
         const image = document.createElement('img');
         image.src = movie.image_url;
-        const bestMovie = document.getElementById('best-score')
+        bestMovie.appendChild(image)
+        image.setAttribute("data-filmid", movie.id)
+        image.addEventListener('click', openModal)
         bestMovie.appendChild(image)
         }
         })}
 
-function getId(){
-    fetch(baseUrl)
-    .then(response => response.json())
-    .then(data => {
-        const movies = data.results;
-        for( movie of movies){
-        const id = movie.id;
-        console.log(id)
-        return id
-        }})
-    }
-
-function getFilmsForCategory(genre) {
-     fetch(baseUrl + '?&genre=' + genre + '&sort_by=-imdb_score')
+function getFilmsForCategory(genre, page) {
+     const films = document.getElementById(genre)
+     films.replaceChildren()
+     fetch(baseUrl + '?&genre=' + genre + '&sort_by=-imdb_score&page=' + page)
     .then(response => response.json())
     .then(data => {
         const movies = data.results;
         for( movie of movies){
         const image = document.createElement('img');
         image.src = movie.image_url;
-        const actionMovie = document.getElementById(genre)
-        actionMovie.appendChild(image)
+        image.setAttribute("data-filmid", movie.id)
+        image.addEventListener('click', openModal)
+        films.appendChild(image)
         }
         })}
 
-function filmInformation(){
- fetch(baseUrl + '1508669')
+function filmInformation(id){
+ fetch(baseUrl + id)
     .then(response => response.json())
     .then(data => {
          const information = document.getElementById('information')
          information.innerHTML = `
-            <img class="modal-img" src="${data.image_url}" alt="" />
-            <p>title: ${data.title}</p>
-            <p>genre: ${data.genres}</p>
-            <p>released: ${data.date_published}</p>
-            <p>vote: ${data.avg_vote}</p>
-            <p>imdb score: ${data.imdb_score}</p>
-            <p>director: ${data.directors}</p>
-            <p>actors: ${data.actors}</p>
-            <p>duration: ${data.duration} min</p>
-            <p>country: ${data.countries}</p>
-            <p>gross income: ${data.worldwide_gross_income}</p>
-            <p>plot: ${data.long_description}</p>
+            <img class="js-modal" src="${data.image_url}" alt="affiche de film" />
+            <p>Titre: ${data.title}</p>
+            <p>Genre: ${data.genres}</p>
+            <p>Date de sortie: ${data.date_published}</p>
+            <p>Rated: ${data.rated}</p>
+            <p>Imdb score: ${data.imdb_score}</p>
+            <p>Réalisateur : ${data.directors}</p>
+            <p>Acteurs: ${data.actors}</p>
+            <p>Durée: ${data.duration} min</p>
+            <p>Pays: ${data.countries}</p>
+            <p>Box office: ${data.worldwide_gross_income}</p>
+            <p>Synopsis: ${data.long_description}</p>
         `;
         })
 }
-
-
 /* creation de la modale */
 let modal = null;
 const openModal = function (e) {
     e.preventDefault();
-    const target = document.querySelector(e.target.getAttribute("href"));
+    const target = document.querySelector("#modal");
+    const id = e.target.dataset.filmid
     target.style.display = "block";
     target.removeAttribute('aria-hidden');
     target.setAttribute('aria-modal', 'true');
 
-    filmInformation()
+    filmInformation(id)
+
     modal = target;
     modal.addEventListener('click', closeModal);
-    modal.querySelector('.js-modal-close').addEventListener('click', closeModal);
-    modal.querySelector('.js-modal-stop').addEventListener('click', stopPropagation);
+    modal.querySelector('.modal-close').addEventListener('click', closeModal);
+    modal.querySelector('.modal-stop').addEventListener('click', stopPropagation);
     }
 
 const closeModal = function(e){
@@ -104,7 +94,7 @@ const closeModal = function(e){
     modal.setAttribute('aria-hidden', 'true');
     modal.removeAttribute('aria-modal', 'true');
     modal.removeEventListener('click', closeModal);
-    modal.querySelector('.js-modal-close').removeEventListener('click', closeModal);
+    modal.querySelector('.modal-close').removeEventListener('click', closeModal);
     modal = null;
 }
 
@@ -112,13 +102,12 @@ const stopPropagation = function(e) {
     e.stopPropagation();
     }
 
-document.querySelectorAll('.js-modal').forEach(a => {
-    a.addEventListener('click', openModal)
-})
-
-/* creation carroussel*/
 
 getBestMovies();
-getFilmsForCategory('animation');
-getFilmsForCategory('thriller');
-getFilmsForCategory('action');
+getFilmsForCategory('animation', "1");
+getFilmsForCategory('thriller', "1");
+getFilmsForCategory('action', "1");
+
+document.querySelectorAll('.js-modal img').forEach(a => {
+    a.addEventListener('click', openModal)
+})
